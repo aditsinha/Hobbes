b1dc9226e0e05f31a74b361233ea51e80c8bc927
Ubuntu <ubuntu@ip-172-31-16-125.ec2.internal>
2016-04-18 21:50:39 +0000

    mvp

--- src/main/java/com/hobbes/wstore/FileByteChangesCoordinator.java
+++ src/main/java/com/hobbes/wstore/FileByteChangesCoordinator.java
@@ -8,19 +8,26 @@ public class FileByteChangesCoordinator {
   	private Map<Path, FileByteChanges> table;
 	private int size;
 	private FileSystem fileSystem;
+	private Path[] lru;
+	private int currLeast;
+	private int currIndex;
 
 	public FileByteChangesCoordinator(FileSystem fileSystem, int size) {
 		table = new HashMap<Path, FileByteChanges>();
 		this.size = size;
 		this.fileSystem = fileSystem;
+		lru = new Path[size];
+		currLeast = currIndex = 0;
 	}
 
 	public FileByteChanges insert(Path dataFile, Path logFile) {
 		FileByteChanges fbc = new FileByteChangesDeque(fileSystem, dataFile, logFile);
-		table.put(dataFile, fbc);
-		if(t.size() > size) {
+		if(t.size() == size) {
 			evict();
 		}
+		table.put(dataFile, fbc);
+		lru[currIndex] = dataFile;
+		currIndex = (currIndex + 1) % size;
 		return fbc;
 	}
 
@@ -40,4 +47,26 @@ public class FileByteChangesCoordinator {
 		FileByteChangesDeque d = read(dataFile, logFile);
 		d.add(b);
 	}
+
+	public FileByteChanges evict() {
+		Path least = lru[currLeast];
+		lru[currLeast] = null;
+		FileByteChanges ret = table.remove(least);
+		ret.writeLog();
+		currLeast = (currLeast + 1) % size;
+	}
+
+	public void tableFlush() {
+		for(Map.Entry<Path, FileByteChanges> entry : table.entrySet()) {
+			entry.getValue().writeLog();
+			table.remove(entry.getKey())
+		}
+	
+		for(int i=0; i < size; i++) {
+			lru[i] = null;
+		}
+		currLeast = currIndex = 0;
+	}
+
+		
 }
